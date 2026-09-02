49aeb6d93b16bc49e530247eb624fc31fa0ee810
Lorenzo <lorenzopalena1806@gmail.com>
2026-09-01 21:28:34 -0300

    chore: remove obsolete PremiumManager from user details page

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { SuspendUserButton } from '@/components/admin/SuspendUserButton';
 import { SubscriptionManager } from '@/components/admin/SubscriptionManager';
 import { MaterialManager } from '@/components/admin/MaterialManager';
-import { PremiumManager } from '@/components/admin/PremiumManager';
+
 
 export const dynamic = 'force-dynamic';
 
@@ -165,7 +165,7 @@ export default async function AdminUserDetailsPage({ params }: { params: Promise
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             <SubscriptionManager userId={profile.id} expiresAt={profile.subscription_expires_at} />
             <MaterialManager userId={profile.id} currentStatus={profile.material_status} />
-            <PremiumManager userId={profile.id} isPremium={profile.is_premium} />
+
           </div>
 
           {/* Active Offers */}
