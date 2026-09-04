f3bbcc238cf7e60bffe11a1b459af3d7a29c8099
Lorenzo <lorenzopalena1806@gmail.com>
2026-09-04 16:20:09 -0300

    remove test files

--- test_map.ts
+++
@@ -1,7 +0,0 @@
-import { extractCoordinatesFromMapsUrl } from './app/actions/map';
-
-async function run() {
-  const url = 'https://maps.app.goo.gl/xxx'; // Wait, I don't have the user's shortlink. I'll just see what happens.
-  console.log("Testing generic maps link...");
-}
-run();
--- test_redirect.ts
+++
@@ -1,7 +0,0 @@
-async function test() {
-  const url = 'https://maps.app.goo.gl/uX3L3zRjFqTvx9Fv8'; // random example shortlink
-  const res = await fetch(url, { redirect: 'manual' });
-  console.log('Status:', res.status);
-  console.log('Location:', res.headers.get('location'));
-}
-test();
